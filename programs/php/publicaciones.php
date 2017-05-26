a19a2b4bc9daf7299b7d5b8373771afc9dfe2f50
Unknown <isaacsantoyo@gmail.com>
2017-05-26 11:03:28 -0500

    Falta likes y mecaga

--- programs/php/publicaciones.php
+++ programs/php/publicaciones.php
@@ -1,4 +1,5 @@
 <?php
+ include("conexionMysql.php");
 	session_start();
 echo " <!DOCTYPE html>
 	<html lang='es'>
@@ -7,16 +8,68 @@ echo " <!DOCTYPE html>
 		<title>Publicaciones</title>
 	</head>
 	<body>";
-$conn=mysqli_connect('localhost','root','','NavyBreak');
+$conexion=mysqli_connect('localhost','root','','NavyBreak');
+//OJO: PARA SUBIR PUBLICAIONES Y COMENTARIOS, ALGUNOS DATOS SE TENDRAN QUE ENVIAR QUE ENVIAR POR METODO POST CON UN CIERTO NOMBRE, INCLUSO ALGUNOS DEBERAN SER HIDDEN, SI LO CREEN NECESARIO.
+if(isset($POST['comment']))
+{
+	$idcom=0;
+	$idpost=$_POST['post'];
+	$idus=$_POST['user'];
+	$com=$_POST['comme'];
+	$comti=date();
+	$likes=0;
+	$mecaga=0;
+	$verificar_id_com=mysqli_query($conexion, "SELECT * FROM COMMENT WHERE id_comment='$idcom'");
+	while(mysqli_num_rows($verificar_id_com)>0)
+		$idcom++;
+	//Inserta publicaciones a Base de datos
+	$coma="INSERT INTO COMMENT(id_comment,id_postcoment,id_usercomment,comment_text,comment_time,likes,mecaga) VALUES ('$idcom','$idpost','$idus','$com','$comti','$likes','$mecaga')";
+	$sub=mysqli_query($conexion,$coma);
+}
+if (isset($POST['public']))
+{
+	$idpos=0;
+	$idus=$_POST['user'];
+	$pub=$_POST['public'];
+	$pubti=date();
+	$verificar_id_post=mysqli_query($conexion, "SELECT * FROM POST WHERE id_post='$idpos'");
+	while(mysqli_num_rows($verificar_id_post)>0)
+		$idpos++;
+	//Inserta publicaciones a Base de datos
+	$publ="INSERT INTO POST(id_post,id_userpost,post_text,post_time) VALUES ('$idpos','$idus','$pub','$pubti')";
+	$sub1=mysqli_query($conexion,$publ);
+}
 echo "<table>";
-//Mando a llamar las publicaciones relacionandolas con el el nombre del usuario pero si agregamos ID al usuario habra que cambiar Usuario por Id_Usuario
-	$query="select * from POST join USER on POST.id_user=USER.id_user order by post_time DESC;";
-	$res=mysqli_query($conn,$query);
+//Este query obtiene las publicaciones con username, el texto y el tiempo en que se realizo
+	$querypub="select username,post_text,post_time from POST join USER on POST.id_userpost=USER.id_user order by post_time DESC;";
+	$res=mysqli_query($conexion,$querypub);
 	$fila=mysqli_fetch_assoc($res);
+//Este otro query obtiene los comentarios; username, texto, tiempo, likes y mecaga que componen al comentario. EL WHERE SE ENCARGA DE ASEGURARSE DE QUE LOS COMENTARIOS SEAN DE LA PUBLICACION Y EVITAR QUE TODOS LOS COMENTARIOS DE TODAS LAS PUBLICACIONES ESTEN EN UNA PUBLICACION
+	$querycom="select username,comment_text,comment_time,likes,mecaga from COMMENT join POST on COMMENT.id_postcomment=POST.id_post join USER on COMMENT.id_usercomment=USER.id_user WHERE id_post=id_postcomment order by post_time DESC;";
+	$resc=mysqli_query($conexion,$querycom);
+	$comm=mysqli_fetch_assoc($resc);
 while($fila)
 {
-	echo $fila['username'].$fila['post_time'];
+	echo $fila['username'];
+	echo "<br/>";
+	echo $fila['post_time'];
+	echo "<br/>";
 	echo $fila['post_text'];
+	echo "<br/>";
+	//Muestra primero las publicaciones y despues los comentarios, hay br para que se muestren los datos, pero se pueden quitar.
+	while($comm)
+	{
+		echo $comm['username'];
+		echo "<br/>";
+		echo $comm['comment_text'];
+		echo $comm['comment_time'];
+		echo "<br/>";
+		echo $comm['likes'];
+		echo "<br/>";
+		echo $comm['mecaga'];
+		echo "<br/>";
+		$comm=mysqli_fetch_assoc($resc);
+	}
 	$fila=mysqli_fetch_assoc($res);
 }
 session_unset();
